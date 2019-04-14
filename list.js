bcbca565e62d928b66dc27e36cf238779a2015df
dustinpfister <dustin.pfister@gmail.com>
2019-04-14 16:16:57 -0400

    list.js

---
+++ list.js
@@ -0,0 +1,29 @@
+// just a way to serve the html folder
+let express = require('express'),
+app = express(),
+fs = require('fs'),
+port = process.env.PORT || process.argv[2] || 8080;
+
+// host forpost and js folders as static
+app.use('/forpost', express.static('html/forpost'));
+app.use('/js', express.static('html/js'));
+
+// generate list of examples for root
+app.get('/', function (req, res) {
+    fs.readdir('./html/forpost', function (e, files) {
+        if (e) {
+            res.send(e.message);
+        } else {
+            let html = '<h1>VUE.JS EXAMPLES:</h1>';
+            files.forEach((folder) => {
+                html += '<a href=\"\/forpost\/' + folder + '\"><p>' + folder + '<\/p>'
+            });
+            res.send(html);
+        }
+    });
+});
+
+// start server
+app.listen(port, function () {
+    console.log('list examples server up on port: ' + port);
+});
