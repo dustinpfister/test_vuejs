1ce0dc4329bef99be2b8a1f61f0e1a5e219762e3
dustinpfister <dustin.pfister@gmail.com>
2019-04-14 16:04:59 -0400

    first commit

---
+++ static.js
@@ -0,0 +1,11 @@
+// just a way to serve the html folder
+let express = require('express'),
+path = require('path'),
+app = express(),
+port = process.env.PORT || process.argv[2] || 8080;
+ 
+app.use('/', express.static('html'));
+
+app.listen(port, function () {
+    console.log('static server up on port: ' + port);
+});
