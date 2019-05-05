026576b93d58c455a4aa92bccd5f5076c2accd0c
dustinpfister <dustin.pfister@gmail.com>
2019-05-05 10:41:49 -0400

    clean up

--- middleware/index_forpost.js
+++ middleware/index_forpost.js
@@ -14,6 +14,7 @@ module.exports = (opt) => {
     let router = express.Router();
     router.get('*', [
 
+            // get files list
             (req, res, next) => {
                 fs.readdir(opt.dir, (e, files) => {
                     if (e) {
@@ -25,8 +26,8 @@ module.exports = (opt) => {
                 });
             },
 
+            // gen html
             (req, res, next) => {
-
                 let html = '<h1>VUE.JS EXAMPLES:</h1>',
                 len = req.files.length,
                 i = 0,
@@ -36,41 +37,23 @@ module.exports = (opt) => {
                         res.send(html);
                     }
                 };
-
                 req.files.forEach((fn) => {
-
                     fs.stat(path.join(opt.dir, fn), (e, stats) => {
-
-                        //html += '<a href=\"\/forpost\/' + folder + '\"><p>' + folder + '<\/p>';
-
                         if (stats.isDirectory()) {
-
                             fs.readdir(path.join(opt.dir, fn), (e, files) => {
-
                                 html += '<ul><li>' + fn + ':<ul>';
-
                                 files.forEach((pfn) => {
-
                                     html += '<li><a href="/' + opt.folderName + '/' + fn + '/' + pfn + '">' + pfn + '<\/a><\/li>'
-
                                 });
-
                                 html += '</ul></li></ul>'
-
                                 step();
-
                             });
-
                         } else {
-
                             html += '<p>' + fn + '<\/p>';
                             step();
-
                         }
-
                     });
                 });
-
             }
 
         ]);
