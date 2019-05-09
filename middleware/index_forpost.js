f1a5612b1a9423f14ba76d94d2f3828c30ff3d73
dustinpfister <dustin.pfister@gmail.com>
2019-05-09 11:29:36 -0400

    fixed weird thing with google translate

--- middleware/index_forpost.js
+++ middleware/index_forpost.js
@@ -28,12 +28,13 @@ module.exports = (opt) => {
 
             // gen html
             (req, res, next) => {
-                let html = '<h1>VUE.JS EXAMPLES:</h1>',
+                let html = '<body><h1>VUE.JS EXAMPLES:</h1>',
                 len = req.files.length,
                 i = 0,
                 step = () => {
                     i += 1;
                     if (i === len) {
+                        html += '<\/body>';
                         res.send(html);
                     }
                 };
@@ -41,15 +42,18 @@ module.exports = (opt) => {
                     fs.stat(path.join(opt.dir, fn), (e, stats) => {
                         if (stats.isDirectory()) {
                             fs.readdir(path.join(opt.dir, fn), (e, files) => {
-                                html += '<ul><li>' + fn + ':<ul>';
+                                //html += '<ul><li>' + fn + ' <ul>';
+								html += '<h2>'+fn + ' examples: <\/h2>';
                                 files.forEach((pfn) => {
-                                    html += '<li><a href="/' + opt.folderName + '/' + fn + '/' + pfn + '">' + pfn + '<\/a><\/li>'
+                                    html += '<li><a href="/' + opt.folderName + '/' + fn + '/' + pfn + '">' + pfn + '<\/a><\/li>';
+                                    //html += '<p>' + path.basename(pfn,path.extname(pfn)).replace(/-|_/g,' ') + '<\/p> ';
+									//html += '<li><\/li> ';
                                 });
-                                html += '</ul></li></ul>'
+                                //html += '</ul><\/li><\/ul>';
                                 step();
                             });
                         } else {
-                            html += '<p>' + fn + '<\/p>';
+                            html += '<p>' + fn + '<\/p ';
                             step();
                         }
                     });
