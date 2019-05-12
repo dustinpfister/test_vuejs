129399d1253627bd93eca836629792a35aa14844
dustinpfister <dustin.pfister@gmail.com>
2019-05-12 13:52:47 -0400

    list example

---
+++ public/forpost/vuejs-render/list.js
@@ -0,0 +1,30 @@
+var vm = new Vue(
+ 
+    {
+        el: '#bar',
+ 
+        render: function (createElement){
+ 
+            var i = 0,
+            len = 10,
+            children = [];
+ 
+            // some javaScript
+            figForI = function (i){
+                return (i - len / 2) / len * Math.pow(2, i);
+            };
+ 
+            // create vNodes
+            while (i < len){
+                children.push(createElement('li', i + ') : ' + figForI(i)));
+                i += 1;
+            }
+ 
+            // return ui with vNodes
+            return createElement('ul', children);
+ 
+        }
+ 
+    }
+ 
+);
