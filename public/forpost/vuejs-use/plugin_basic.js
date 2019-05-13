9ce9a4c89fdc65126efabdbb5eed50402f211d2e
dustinpfister <dustin.pfister@gmail.com>
2019-05-13 11:59:18 -0400

    basic example working okay

---
+++ public/forpost/vuejs-use/plugin_basic.js
@@ -0,0 +1,11 @@
+var basic ={
+    // Vue.js plug in must have an install property
+    // that is a function. Vue and an optional options
+    // object will be passed
+    install: function (Vue, opt){
+        // can define instance methods this way
+        Vue.prototype.$foo = function (func){
+            func('bar');
+        }
+    }
+};
--- public/forpost/vuejs-use/plugin_http_get.js
+++ public/forpost/vuejs-use/plugin_http_get.js
@@ -1,13 +1,63 @@
-var http = (function ()
+// Simple http get client for vuejs
+// using XMLHttpRequest
+var httpGet = (function ()
 {
 
-    var api = {};
-	
-	// must have an install method
-	api.install = function(){
-		
-		
-	};
+    var api =
+    {
+
+        get: function (opt)
+        {
+
+            opt = opt || {};
+            opt.url = opt.url || '/';
+            opt.beforeSend = opt.beforeSend || function (xhr)  {};
+            opt.onDone = opt.onDone || function (res)
+            {
+                console.log(res);
+            }
+            opt.onError = opt.onError || function ()
+            {
+                console.log(this);
+            }
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', opt.url);
+            opt.beforeSend.call(this, xhr);
+            xhr.onreadystatechange = function ()
+            {
+
+                console.log(this.readyState, this.status);
+
+                if (this.readyState === 4 && this.status === 200)
+                {
+                    opt.onDone.call(this, this.response);
+                }
+
+                if (this.readyState === 4 && this.status === !200)
+                {
+                    opt.onError.call(this, this);
+                };
+
+            }
+
+            xhr.send();
+        }
+    };
+
+    // must have an install method
+    api.install = function (Vue, opt)
+    {
+
+        opt = opt || {};
+        opt.baseUrl = opt.baseUrl || '/';
+
+        Vue.prototype.$httpGet = function (getOpt)
+        {
+            api.get(getOpt);
+        }
+
+    };
 
     return api;
 
