530e4f67d420de74e6f39c3c92e7252410e928c0
dustinpfister <dustin.pfister@gmail.com>
2019-05-13 12:01:54 -0400

    more basic basic plugin

--- public/forpost/vuejs-use/plugin_basic.js
+++ public/forpost/vuejs-use/plugin_basic.js
@@ -1,11 +1,11 @@
 var basic ={
-    // Vue.js plug in must have an install property
+    // a Vue.js plug in must have an install property
     // that is a function. Vue and an optional options
     // object will be passed
     install: function (Vue, opt){
         // can define instance methods this way
-        Vue.prototype.$foo = function (func){
-            func('bar');
+        Vue.prototype.$foo = function (){
+            return 'foobar';
         }
     }
 };
