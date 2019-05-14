72356a2f939392a5ca49837668c140a340cb0389
dustinpfister <dustin.pfister@gmail.com>
2019-05-14 10:18:39 -0400

    text.js example

---
+++ public/forpost/vuejs-directive/basic.js
@@ -0,0 +1,7 @@
+
+var vm = new Vue({
+        el: '#bar',
+        data: {
+            mess: 'bar'
+        }
+    });
---
+++ public/forpost/vuejs-directive/text.js
@@ -0,0 +1,9 @@
+
+var vm = new Vue({
+        el: '#header',
+        data: {
+            mess_h1: 'This is a v-text dirrective example',
+            mess_p: 'Uisng the v-text and v-bind:style dirrectives here',
+            style_red: 'color:red;'
+        }
+    });
