545bef0d580977da4f7ef70295270c92ea36aeb8
dustinpfister <dustin.pfister@gmail.com>
2019-05-16 11:06:29 -0400

    prop example

---
+++ public/forpost/vuejs-component/props.js
@@ -0,0 +1,19 @@
+// A Basic step Component
+Vue.component('step', {
+    props: ['si'],
+    template: '<div><button v-on:click="step">step</button> i: {{ i }} </div>',
+    data: function (a) {
+        return {
+            i: this.si === undefined ? 0 : this.si
+        }
+    },
+    methods: {
+        step: function (e) {
+            this.$data.i += 1;
+        }
+    }
+});
+
+new Vue({
+    el: '#components-demo'
+})
