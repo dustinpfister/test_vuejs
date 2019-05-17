54bfb9fee2221acf4fb018b3a9812a96c3579a88
dustinpfister <dustin.pfister@gmail.com>
2019-05-17 12:17:30 -0400

    bue-slot basic example

--- public/forpost/vuejs-slot/basic.js
+++ public/forpost/vuejs-slot/basic.js
@@ -1,6 +1,14 @@
+// very simple component with a slot
+Vue.component('foo', {
+    template: '<div><slot></slot></div>'
+})
+
+// a Vue instance using the component element
+// that has some inner content in the form of a
+// simple text node
 new Vue({
     el: '#demo-data',
-    template: '<p>{{foo}}</p>',
+    template: '<foo>hello</foo>',
     data: {
         foo: 'bar'
     }
