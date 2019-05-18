77c7e0d8167462528e1f24e61e9507efa82a18ab
dustinpfister <dustin.pfister@gmail.com>
2019-05-17 20:43:00 -0400

    data access example

--- public/forpost/vuejs-data/access.js
+++ public/forpost/vuejs-data/access.js
@@ -1,7 +1,14 @@
 new Vue({
     el: '#demo-data',
-    template: '<p>{{foo}}</p>',
+    template: '<div><input type="button" value="step" v-on:click="bar" ><span> i: {{ i }}</span></div>',
     data: {
-        foo: 'bar'
+        i: 0
+    },
+    methods: {
+        bar: function () {
+            // the data object can be accessed via
+            // the this.$data property
+            this.$data.i += 1;
+        }
     }
 });
