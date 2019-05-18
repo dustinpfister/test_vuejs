d9fcdce1c49d3fe07d496d166a16121ae6dcb64e
dustinpfister <dustin.pfister@gmail.com>
2019-05-17 20:59:41 -0400

    clean up

--- public/forpost/vuejs-slot/named.js
+++ public/forpost/vuejs-slot/named.js
@@ -2,11 +2,15 @@ Vue.component('foo', {
     template: '<div>' +
     '<slot name="title" ></slot>' +
     '<slot></slot>' +
-    '<slot name="baz"></slot>' +
+    '<slot name="footer"></slot>' +
     '</div>'
 })
 
 new Vue({
     el: '#demo-slot',
-    template: '<div><foo v-slot:title>Slots are fun</foo><foo>If no name is given then it is the defulat slot</foo></div>'
+    template: '<div>' +
+    '<foo v-slot:title>Slots are fun</foo>' +
+    '<foo>If no name is given then it is the defulat slot</foo>' +
+    '<foo v-slot:footer>So names help with more than one slot</foo>' +
+    '</div>'
 });
