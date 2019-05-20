42b5955dd1a373666c25e6817427a75d18f10d2e
dustinpfister <dustin.pfister@gmail.com>
2019-05-20 12:25:28 -0400

    mod key demo done

--- public/forpost/vuejs-methods/mod_key.js
+++ public/forpost/vuejs-methods/mod_key.js
@@ -1,15 +1,20 @@
 new Vue({
     el: '#demo-methods',
     template: '<div>' +
-    '<input v-on:keyup.13="submit" type="text" v-bind:value="mess" >' +
+    '<input v-on:keyup.13="submit" v-on:change="change" type="text" v-bind:value="name" >' +
     '<input type="button" v-on:click="submit" value="submit">' +
+    '<ul><li> name: {{ name }}</li><li>mess: {{ mess}}</li></ul>' +
     '</div>',
     data: {
-        mess: 'foo'
+        name: 'Dustin',
+        mess: ''
     },
     methods: {
+        change: function (e) {
+            this.$data.name = e.target.value;
+        },
         submit: function (e) {
-            console.log(this.$data.mess)
+            this.$data.mess = 'Hello ' + this.$data.name;
         }
     }
 });
