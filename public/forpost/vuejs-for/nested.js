29b0111acf6d171f8d620cb204ac696224602272
dustinpfister <dustin.pfister@gmail.com>
2019-05-20 13:15:30 -0400

    links to google

--- public/forpost/vuejs-for/nested.js
+++ public/forpost/vuejs-for/nested.js
@@ -2,7 +2,10 @@ new Vue({
     el: '#list',
     template: '<ul>' +
     '<li v-for="(cat,ci) in cats" >{{ ci+1 }} ) {{ cat.name }}<ul>' +
-    '<li v-for="(kw,ki) in cat.keywords" >{{ ( ci + 1 ) + \'.\' + ( ki + 1) }} ) {{ kw }}</li>' +
+    '<li v-for="(kw,ki) in cat.keywords" >' +
+    '<a v-bind:href="\'https://www.google.com/search?q=\' + kw">' +
+    '{{ ( ci + 1 ) + \'.\' + ( ki + 1) }} ) {{ kw }}' +
+    '</a></li>' +
     '</ul></li>' +
     '</ul>',
     data: {
