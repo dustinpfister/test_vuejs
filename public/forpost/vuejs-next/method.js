69656ecc7523bf07acd3f4b1a6d3a15be208875c
dustinpfister <dustin.pfister@gmail.com>
2019-05-23 19:16:32 -0400

    next tick instnace method

---
+++ public/forpost/vuejs-next/method.js
@@ -0,0 +1,20 @@
+var vm = new Vue({
+        el: '#demo-nexttick',
+        template: '<p>{{ mess }}</p>',
+        data: {
+            mess: 'bar'
+        },
+        methods: {
+            setMess: function (mess) {
+                var self = this;
+                self.mess = mess;
+                // The $nextTick instance method can
+                // also be used
+                self.$nextTick(function () {
+                    console.log(self.$el.textContent); // 'foo'
+                });
+            }
+        }
+    });
+
+vm.setMess('foo');
