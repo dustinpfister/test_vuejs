c559b61223f5a896098a3fcdca791cb7df2e8ec8
dustinpfister <dustin.pfister@gmail.com>
2019-05-24 16:50:36 -0400

    add_events demo working okay

--- public/forpost/vuejs-render/add_events.js
+++ public/forpost/vuejs-render/add_events.js
@@ -1,14 +1,24 @@
-new Vue(
-    {
-        el: '#demo-render',
-        render: function (createElement){
-            return createElement('div', {on:{click:this.bar}},'foo');
-        },
-		methods: {
-			bar: function(){
-				console.log('foobar')
-			}
-		}
+new Vue({
+    el: '#demo-render',
+    data: {
+        frame: 0,
+        maxFrame: 10
+    },
+    render: function (createElement) {
+        return createElement('input', {
+            attrs: {
+                type: 'button',
+                value: 'step ' + this.$data.frame + '/' + this.$data.maxFrame
+            },
+            on: {
+                click: this.step
+            }
+        });
+    },
+    methods: {
+        step: function () {
+            this.$data.frame += 1;
+            this.$data.frame %= this.$data.maxFrame;
+        }
     }
-);
-
+});
