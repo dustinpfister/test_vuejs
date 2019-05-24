4c24d4e226b7ed4fd7d17be229546fc88ee993cb
dustinpfister <dustin.pfister@gmail.com>
2019-05-24 06:51:02 -0400

    basic example looking good

--- public/forpost/vuejs-lifecycle-created/basic.js
+++ public/forpost/vuejs-lifecycle-created/basic.js
@@ -1,10 +1,22 @@
 var vm = new Vue({
         el: '#demo-lifecycle-created',
-        template: '<p>{{ n }}</p>',
+        template: '<p>n: {{ n }}</p>',
         data: {
             n: 4
         },
+
+        // created lifecycle hook
         created: function () {
-            console.log('created');
+            console.log('**** CREATED ****');
+            console.log(this.$data.n); // 4
+            console.log(this.$el); // undefined
+            console.log('*****************');
+        },
+
+        mounted: function () {
+            console.log('**** MOUNTED ****');
+            console.log(this.$el.textContent); // n: 4
+            console.log('*****************');
         }
+
     });
