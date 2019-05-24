c587cebf50afc35ec06cdaff337a22ac30485a3e
dustinpfister <dustin.pfister@gmail.com>
2019-05-24 09:58:49 -0400

    el_none

--- public/forpost/vuejs-lifecycle-created/basic.js
+++ public/forpost/vuejs-lifecycle-created/basic.js
@@ -1,22 +1,20 @@
-var vm = new Vue({
-        el: '#demo-lifecycle-created',
-        template: '<p>n: {{ n }}</p>',
-        data: {
-            n: 4
-        },
-
-        // created lifecycle hook
-        created: function () {
-            console.log('**** CREATED ****');
-            console.log(this.$data.n); // 4
-            console.log(this.$el); // undefined
-            console.log('*****************');
-        },
-
-        mounted: function () {
-            console.log('**** MOUNTED ****');
-            console.log(this.$el.textContent); // n: 4
-            console.log('*****************');
-        }
-
-    });
+new Vue({
+    el: '#demo-lifecycle-created',
+    template: '<p>n: {{ n }}</p>',
+    data: {
+        n: 4
+    },
+    // created lifecycle hook
+    created: function () {
+        console.log('**** CREATED ****');
+        console.log(this.$data.n); // 4
+        console.log(this.$el); // undefined
+        console.log('*****************');
+    },
+    // mounted lifecycle hook
+    mounted: function () {
+        console.log('**** MOUNTED ****');
+        console.log(this.$el.textContent); // n: 4
+        console.log('*****************');
+    }
+});
---
+++ public/forpost/vuejs-lifecycle-created/sync.js
@@ -0,0 +1,27 @@
+var vm = new Vue({
+        template: '<p>time: {{ time }}</p>',
+        data: {
+            st: 0,
+            time: 0
+        },
+        // created lifecycle hook
+        created: function () {
+            // called right away
+            console.log('created');
+            this.$data.st = Date.now();
+            //i = Math.pow(10,6);
+            //while (i--) {};
+            //this.$data.time = Date.now() - st;
+        },
+        // mounted lifecycle hook
+        mounted: function () {
+            // called after $mount because no
+            // el option is given
+            console.log('mounted');
+            this.$data.time = Date.now() - this.$data.st;
+        }
+    });
+
+setTimeout(function () {
+    vm.$mount('#demo-lifecycle-created')
+}, 2500);
