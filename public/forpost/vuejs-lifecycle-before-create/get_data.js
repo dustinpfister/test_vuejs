0468b95a899a548db239577fb52d670ce8dcefe4
dustinpfister <dustin.pfister@gmail.com>
2019-05-26 12:37:08 -0400

    clean up

--- public/forpost/vuejs-lifecycle-before-create/get_data.js
+++ public/forpost/vuejs-lifecycle-before-create/get_data.js
@@ -5,30 +5,28 @@ new Vue({
         n: null,
         mess: ''
     },
-
     // default create hook
-    beforeCreate: function () {
+    beforeCreate: ()=> {
         // define hard coded data object
         this.hardData = {
             n: 21,
             mess: 'hard coded data is used'
         };
     },
-
     // create hook
     created: function () {
         var self = this;
         // fetch data
         fetch('/data')
-        .then(function (res) {
+        .then((res)=> {
             return res.json();
         })
-        .then(function (data) {
+        .then((data)=> {
             // if all goes well use that
             self.$data.n = data.n;
             self.$data.mess = 'Got data from back end';
         })
-        .catch (function (e) {
+        .catch ((e)=> {
             // else use hard data
             self.$data.n = self.hardData.n;
             self.$data.mess = self.hardData.mess + ' : ' + e.message;
