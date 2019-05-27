bd06f831a3ed9f366273e704f2675b6888ceaf30
dustinpfister <dustin.pfister@gmail.com>
2019-05-26 21:02:29 -0400

    mount

---
+++ public/forpost/vuejs-lifecycle-mounted/mount.js
@@ -0,0 +1,14 @@
+var vm = new Vue({
+        template: '<p>n: {{ n }}</p>',
+        data: {
+            n: 4
+        },
+        // mounted lifecycle hook
+        mounted: function () {
+            console.log(this.$el.textContent); // n: 4
+        }
+    });
+
+setTimeout(function () {
+    vm.$mount('#demo-lifecycle-mounted');
+}, 3000);
