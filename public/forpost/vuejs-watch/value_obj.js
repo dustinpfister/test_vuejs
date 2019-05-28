b18508e71df357e390b925bc3a2ac6f0d24fac5f
dustinpfister <dustin.pfister@gmail.com>
2019-05-28 10:31:35 -0400

    mounted hook

--- public/forpost/vuejs-watch/value_obj.js
+++ public/forpost/vuejs-watch/value_obj.js
@@ -1,47 +1,43 @@
-var vm = new Vue({
-        el: '#demo-watch',
-        template: '<div><p> ( {{ point.x }} , {{ point.y }} )</p>' +
-        '<ul><li v-for=\"m in mess\" >{{ m }}</li></ul></div>',
-        data: {
-            point: {
-                x: 40,
-                y: 2
+new Vue({
+    el: '#demo-watch',
+    template: '<div><p> ( {{ point.x }} , {{ point.y }} )</p>' +
+    '<ul><li v-for=\"m in mess\" >{{ m }}</li></ul></div>',
+    data: {
+        point: {
+            x: 40,
+            y: 2
+        },
+        mess: []
+    },
+    watch: {
+        point: {
+            handler: function (newVal, oldVal) {
+                this.$data.mess.push('Point change to ( ' + newVal.x + ' , ' + newVal.y + ' )');
             },
-            mess: []
-        },
-        watch: {
-            point: {
-                handler: function (newVal, oldVal) {
-                    this.$data.mess.push('Point change to ( ' + newVal.x + ' , ' + newVal.y + ' )');
-                },
-                deep: true
-            }
-        },
-        methods: {
-
-            rand1: function () {
-                this.$data.point.x = 25 + Math.floor(Math.random() * 75);
-                this.$data.point.y = 50;
-            }
-
+            deep: true
         }
-    });
-
-// change manually point
-vm.point.x = 0;
-vm.point.y = 0;
-
-// script that changes point
-var count = 5,
-delay = 500;
-var next = function () {
-
-    if (count) {
+    },
+    methods: {
+        rand1: function () {
+            this.$data.point.x = 25 + Math.floor(Math.random() * 75);
+            this.$data.point.y = 75;
+        }
+    },
+    mounted: function () {
+        var vm = this;
+        // change manually point
+        vm.point.x = 0;
+        vm.point.y = 0;
+        // script that changes point
+        var count = 5,
+        delay = 500;
+        var next = function () {
+            if (count) {
+                setTimeout(next, delay);
+            }
+            vm.rand1();
+            count -= 1;
+        };
         setTimeout(next, delay);
     }
-    //vm.point.x = ;
-    //vm.point.y = 25;
-	vm.rand1();
-    count -= 1;
-};
-setTimeout(next, delay);
+});
