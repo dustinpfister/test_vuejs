f44ff8194a142034b6cb4e6d3c89b13b9b9d4115
dustinpfister <dustin.pfister@gmail.com>
2019-06-03 13:53:16 -0400

    demo cleanup

--- public/forpost/vuejs-bind/text.js
+++ public/forpost/vuejs-bind/text.js
@@ -1,11 +1,11 @@
 new Vue({
     el: '#demo-bind',
     template: '<div>' +
-    '<p v-bind:style="red">{{ mess1 }}</p>' +
+    '<p>{{ mess1 }}</p>' +
     '<p v-text=\"mess2\"></p>' +
     '</div>',
     data: {
-        mess1: 'mustachen syntax',
-        mess2: 'text directive'
+        mess1: 'mustache syntax',
+        mess2: 'bind text directive'
     }
 });
