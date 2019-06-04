f31beb0ea90ce6bf9ac85d254db18d8eac5e7b68
dustinpfister <dustin.pfister@gmail.com>
2019-06-03 20:21:46 -0400

    source

--- public/forpost/vuejs-error/all_error.js
+++ public/forpost/vuejs-error/all_error.js
@@ -2,21 +2,26 @@ var vmError = new Vue({
         el: '#demo-error',
         template: '<div>' +
         '<span>Error: {{ error.mess }}</span></br>' +
+        '<span>Source: {{ error.source }}</span></br>' +
         '<span>line,col: {{ error.line }} , {{ error.col }}</span></br>' +
         '</div>',
         data: {
             error: {
-                mess: ''
+                mess: '',
+                source: '',
+                line: '',
+                col: ''
             }
         }
     });
 
-window.onerror = function (message, source, line, col, error) {
-    //console.log('Exception: ', error)
-    vmError.error.mess = message;
+window.onerror = function (mess, source, line, col) {
+    vmError.error.mess = mess;
+    vmError.error.source = source;
     vmError.error.line = line;
     vmError.error.col = col;
 
 };
 
+// trowning an error
 throw new Error('My error');
