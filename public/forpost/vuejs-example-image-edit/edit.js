af7f7d074c1f32ec44a83804f5d0a85a7210aa4d
dustinpfister <dustin.pfister@gmail.com>
2020-07-27 16:08:36 -0400

    rendering grid

--- public/forpost/vuejs-example-image-edit/edit.js
+++ public/forpost/vuejs-example-image-edit/edit.js
@@ -1,12 +1,31 @@
 new Vue({
     el: '#app',
-    render: function(createElement){
-       return createElement('p', 'Hello World');
+    render: function (createElement) {
+        var grid = [],
+        div,
+        cellOpt,
+        i = 0,
+        len = this.$data.width * this.$data.height;
+        while (i < len) {
+            cellOpt = {
+                style: {
+                    position: 'absolute',
+                    width: '32px',
+                    height: '32px',
+                    background: 'red'
+                }
+            };
+            cellOpt.style.left = (i % this.$data.width * 32) + 'px';
+            cellOpt.style.top = (Math.floor(i / this.$data.height) * 32) + 'px';
+            div = createElement('div', cellOpt, '');
+            grid.push(div);
+            i += 1;
+        }
+        return createElement('div', grid);
     },
     data: {
-        
+        width: 4,
+        height: 4,
     },
-    methods: {
-        
-    }
+    methods: {}
 })
