98ca30030a40663e39c196d7ce60dc054c418c85
dustinpfister <dustin.pfister@gmail.com>
2020-10-06 17:13:34 -0400

    clean up

--- public/forpost/vuejs-compile/module.js
+++ public/forpost/vuejs-compile/module.js
@@ -1,24 +1,17 @@
 var Mod = (function () {
-
     var api = {};
-
+    // create a state object
     api.create = function () {
-
         return {
             count: 0
         }
-
     };
-
+    // update a state object
     api.update = function (state) {
-
         state.count += 1;
-
     };
-
+    // a vuejs template
     api.template = '<div><input type="button" value="step" v-on:click="update"><span> {{count}} </span></div>';
-
     return api;
-
 }
     ());
