e3c7eb03221a4a0210b43aea77227656b38bf14c
dustinpfister <dustin.pfister@gmail.com>
2021-01-25 10:14:27 -0500

    createing ids for each box

--- public/forpost/vuejs-example-idle-game/game.js
+++ public/forpost/vuejs-example-idle-game/game.js
@@ -3,22 +3,22 @@ new Vue({
     template: '<div>' +
         '<input type="button" value="mine" v-on:click="click"> <span> {{ money }} </span> <br>' +
         '<div>' +
-            '<div class="wrap_minbox" v-for="min in minerals" > {{ min.type }} </div>' +
+            '<div v-bind:id="min.id" class="wrap_minbox" v-for="min in minerals" > {{ min.type }} </div>' +
         '</div>' +
     '</div>',
     data: {
         money: 0,
         minerals: [
-            {type: 'iron', unitCount: 0, moneyPerUnit: 1},
-            {type: 'copper', unitCount: 0, moneyPerUnit: 2.5},
-            {type: 'silver', unitCount: 0, moneyPerUnit: 7}
+            {id: 'min_iron', type: 'iron', unitCount: 0, moneyPerUnit: 1},
+            {id: 'min_copper', type: 'copper', unitCount: 0, moneyPerUnit: 2.5},
+            {id: 'min_silver', type: 'silver', unitCount: 0, moneyPerUnit: 7}
         ]
     },
     methods: {
         // a button was clicked
         click: function (e) {
             var dat = this.$data;
-            dat.money += 1;
+            //dat.money += 1;
         }
     }
 })
