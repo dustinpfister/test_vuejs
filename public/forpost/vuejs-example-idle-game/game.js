8b6f93e3e48dd02eeeed04bc322cd2839f42ed6d
dustinpfister <dustin.pfister@gmail.com>
2021-01-25 09:46:29 -0500

    very basic click action working

--- public/forpost/vuejs-example-idle-game/game.js
+++ public/forpost/vuejs-example-idle-game/game.js
@@ -1,13 +1,14 @@
 new Vue({
     el: '#app',
-    template: '<div><input type="button" value="mine" v-on:click="click"></div>',
+    template: '<div><input type="button" value="mine" v-on:click="click"> {{ money }} </div>',
     data: {
         money: 0
     },
     methods: {
         // a button was clicked
         click: function (e) {
-            console.log('foo')
+            var dat = this.$data;
+            dat.money += 1;
         }
     }
 })
