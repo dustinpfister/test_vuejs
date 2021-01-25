5b358ce7d2db1c72a5810638a3a461714b3ac9ed
dustinpfister <dustin.pfister@gmail.com>
2021-01-25 10:02:20 -0500

    have a div for each min

--- public/forpost/vuejs-example-idle-game/game.js
+++ public/forpost/vuejs-example-idle-game/game.js
@@ -1,8 +1,18 @@
 new Vue({
     el: '#app',
-    template: '<div><input type="button" value="mine" v-on:click="click"> {{ money }} </div>',
+    template: '<div>' +
+        '<input type="button" value="mine" v-on:click="click"> <span> {{ money }} </span> <br>' +
+        '<div>' +
+            '<div class="wrap_minbox" v-for="min in minerals" > {{ min.type }} </div>' +
+        '</div>' +
+    '</div>',
     data: {
-        money: 0
+        money: 0,
+        minerals: [
+            {type: 'iron', unitCount: 0, moneyPerUnit: 1},
+            {type: 'copper', unitCount: 0, moneyPerUnit: 2.5},
+            {type: 'silver', unitCount: 0, moneyPerUnit: 7}
+        ]
     },
     methods: {
         // a button was clicked
