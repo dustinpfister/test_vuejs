fddc0d14b8b9d9c573595971d8a264020e5bfb7c
dustinpfister <dustin.pfister@gmail.com>
2021-02-01 09:57:18 -0500

    just html files

--- public/forpost/vuejs-if/s2_render.js
+++
@@ -1,9 +0,0 @@
-new Vue({
-    el: '#demo-if',
-    render: function (createElement) {
-        return createElement('div',[createElement('p', typeof this.n === 'number' ? this.n.toFixed(2) : this.n)])
-    },
-    data: {
-        n: Math.PI
-    }
-});
