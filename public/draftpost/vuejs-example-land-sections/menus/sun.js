cc6e860684c242ad74b6547b824d75c579327622
dustinpfister <dustin.pfister@gmail.com>
2021-02-02 10:24:42 -0500

    key down for ne input

--- public/draftpost/vuejs-example-land-sections/menus/sun.js
+++ public/draftpost/vuejs-example-land-sections/menus/sun.js
@@ -8,21 +8,36 @@ Vue.component('menu-sun', {
       var children = [];
       var vm = this;
       if(this.$props.currentMenu === 'sun'){
+          // center button
           children.push(createElement('input', {
               attrs: {
                  type: 'button',
                  value: 'center'
               },
               on: {
-                  click: this.click
+                  click: this.center
+              }
+          }));
+          children.push(createElement('input', {
+              attrs: {
+                 type: 'text',
+                 value: vm.$props.sun.a
+              },
+              on: {
+                  keydown: function(e){
+                      console.log('key down');
+                  }
               }
           }));
       }
       return createElement('div', children);
   },
   methods: {
-    click: function(e){
+    center: function(e){
         this.$emit('set-sunpos-ad', 0, 0);
+    },
+    setPos: function(a, d){
+        this.$emit('set-sunpos-ad', a, d);
     }
   }
 });
