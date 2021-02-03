2d11cdf8daa5b855eb25ac8f9c0caa8e7d5aaa76
dustinpfister <dustin.pfister@gmail.com>
2021-02-03 11:47:27 -0500

    lib folder

---
+++ public/draftpost/vuejs-example-web-assets/lib/webasset.js
@@ -0,0 +1,16 @@
+var WebAsset = (function(){
+
+
+
+    var api = function(opt){
+        var asset = {
+            postCount: 100,
+            avgWordsPerPost: 800,
+            monetize: ['ads']
+        };
+        return asset;
+    };
+
+    return api;
+
+}());
