9afa46e3b0cff6a6a45c37376661afadfb39168b
dustinpfister <dustin.pfister@gmail.com>
2021-02-03 15:08:10 -0500

    displaying info about content

--- public/forpost/vuejs-example-web-assets/comp/webasset-ui-current.js
+++ public/forpost/vuejs-example-web-assets/comp/webasset-ui-current.js
@@ -6,7 +6,10 @@ Vue.component('webassets-ui-current', {
         '<div v-for="asset, index in state.webAssets" class="currentsite">'+
             '<p>Site: </p>'+
             '<p>Worth: {{ format_money(asset.worth) }}, Money Per Tick: {{ format_money(asset.moneyPerTick) }}</p>'+
-            '<p>{{ asset.write.words }}</p>'+
+            '<p>Site has {{ asset.words }} total words accross '+
+                '{{ asset.postCount }} blog posts which is an average of '+
+                '{{ Math.floor(asset.avgWordsPerPost) }} words per post</p>'+
+            '<p>Write new post: {{ asset.write.words }}/{{ asset.write.target }} words done on new post</p>'+
             '<p><button v-on:click="write(index)">Write</button> | <button v-on:click="sell(index)">Sell</button></p>'+
             '<div v-bind:style="\'width:\'+Math.round(asset.secs / asset.secsPerTick * 100)+\'%;height:10px;background:lime;\'"></div>'+
         '</div>'+
--- public/forpost/vuejs-example-web-assets/todo.md
+++ public/forpost/vuejs-example-web-assets/todo.md
@@ -1,8 +1,9 @@
 
 ## 0.1.0 - write new posts for a current asset
 * (done) have a write button for each current web asset
-* clicking the write button will step the write.words count of the asset
-* when write.words == write.target, asset.postCount will step by one, and write.target will be added to asset.words
+* (done) clicking the write button will step the write.words count of the asset
+* (done) when write.words == write.target, asset.postCount will step by one, and write.target will be added to asset.words
+* display post count, and total words
 
 ## 0.0.0 - basic general idea working
 * (done) have a way to create a web asset from nothing
