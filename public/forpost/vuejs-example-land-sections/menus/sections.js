ce7d4b3111a4dc85dd1930900cb6c45c9d621dda
dustinpfister <dustin.pfister@gmail.com>
2021-02-04 16:13:18 -0500

    sections-ui-cellaction comp started

--- public/forpost/vuejs-example-land-sections/menus/sections.js
+++ public/forpost/vuejs-example-land-sections/menus/sections.js
@@ -1,3 +1,9 @@
+Vue.component('sections-ui-cellaction', {
+    template: '<div class="menu_item">'+
+        '<p>Cell Action Select</p>' +
+    '</div>'
+});
+
 Vue.component('menu-sections', {
     props: ['currentMenu', 'sun', 'sections'],
     data: function(){
@@ -7,9 +13,10 @@ Vue.component('menu-sections', {
             cellAction: 0
         }
     },
-    template: '<div v-if="currentMenu === \'sections\'">'+
-        '<sections-ui-select v-bind:section="section" v-on:step-section="step" ></sections-ui-select>'+
-        '<sections-ui-grid v-bind:section="section" v-on:click-cell="clickCell" ></sections-ui-grid>'+
+    template: '<div v-if="currentMenu === \'sections\'">' +
+        '<sections-ui-select v-bind:section="section" v-on:step-section="step" ></sections-ui-select>' +
+        '<sections-ui-cellaction></sections-ui-cellaction>' +
+        '<sections-ui-grid v-bind:section="section" v-on:click-cell="clickCell" ></sections-ui-grid>' +
     '</div>',
     methods: {
         step: function(deltaIndex){
