d85736d7813c4283241fae0b55b1d49569fe41dd
dustinpfister <dustin.pfister@gmail.com>
2021-02-09 12:37:44 -0500

    comp folder and image div grid

--- public/forpost/vuejs-example-image-edit/edit.js
+++ public/forpost/vuejs-example-image-edit/edit.js
@@ -1,42 +1,4 @@
 
-Vue.component('image-div-grid', {
-    props: ['img'],
-    render: function(createElement){
-        var img = this.$props.img,
-        vm = this,
-        divs = [];
-        img.data.forEach(function(px, i){
-            var x = i % img.width,
-            y = Math.floor(i / img.width),
-            color = img.palette[px];
-            var px = createElement('div', {
-                attrs: {
-                    id: 'px_' + x + '_' + y,
-                    class: 'image-div-grid-px',
-                    style: 'left:' + ( x * img.pxSize ) + 'px;top:'+ ( y * img.pxSize ) +';background:' + color + ';'
-                },
-                on:{
-                    click: vm.pxClick
-                }
-            });
-            divs.push(px);
-        });
-        return createElement('div', {
-            attrs: {
-                class: 'image-div-grid'
-            }
-        }, divs);
-    },
-    methods: {
-        pxClick: function(e){
-            var div = e.target,
-            idArr = div.id.split('_')
-            console.log(idArr);
-            this.$emit('px-click', idArr[1], idArr[2],);
-        }
-    }
-});
-
 new Vue({
     el: '#app',
     template: '<div>'+
