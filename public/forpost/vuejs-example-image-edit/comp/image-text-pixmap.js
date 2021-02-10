14af4bd0e5d95a7779f52793d3e6a3caa42e41b6
dustinpfister <dustin.pfister@gmail.com>
2021-02-10 13:00:39 -0500

    validate method

--- public/forpost/vuejs-example-image-edit/comp/image-text-pixmap.js
+++ public/forpost/vuejs-example-image-edit/comp/image-text-pixmap.js
@@ -2,11 +2,14 @@ Vue.component('image-text-pixmap', {
     props: ['imgs'],
     data: function(){
         return {
-            json: ''
+            json: '',
+            vaild: false,
+            mess: ''
         };
     },
     template: '<div class="ui-div">'+
         '<h3>Pixmap JSON</h3>'+
+        '<p>vaild: {{ vaild }}, mess: {{ mess }}</p>' +
         '<button v-on:click="load">load</button><br>'+
         '<textarea cols="70" rows="15" v-text="json" v-on:keyup="keyup"></textarea>'+
     '</div>',
@@ -14,12 +17,32 @@ Vue.component('image-text-pixmap', {
         this.updateText();
     },
     methods: {
+        validate: function(){
+
+            var dat = this.$data;
+            dat.valid = false;
+            try{
+                // the json should parse okay
+                var json = JSON.parse(this.$data.json);
+                dat.valid = true;
+                dat.mess = 'JSON looks good';
+            }catch(e){
+                dat.mess = 'Error parsing JSON';
+            }
+
+        },
         // what to do on a keyup event
         keyup: function(e){
             this.$data.json = e.target.value;
         },
         // emit a 'load-json' event
         load: function(){
+            var dat = this.$data;
+            this.validate();
+            if(dat.valid){
+                this.$emit('load-json', dat.json);
+            }
+/*
             try{
                 // the json should parse okay
                 var json = JSON.parse(this.$data.json);
@@ -27,6 +50,7 @@ Vue.component('image-text-pixmap', {
             }catch(e){
                 this.$emit('load-json-error', 'JSON parse Error');
             }
+*/
         },
         // update textarea
         updateText : function(){
@@ -39,6 +63,8 @@ Vue.component('image-text-pixmap', {
             this.$data.json = JSON.stringify(pixmap);
             //!!! I should not have to do this, but it seems like I have to
             this.$el.querySelectorAll('textarea')[0].value = this.$data.json;
+
+            this.validate();
         }
     }
 });
