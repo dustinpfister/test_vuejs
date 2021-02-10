2df29b7c9e09c06956977405beff36f75ce22fa9
dustinpfister <dustin.pfister@gmail.com>
2021-02-10 08:41:03 -0500

    started image-text-pixmap comp

---
+++ public/forpost/vuejs-example-image-edit/comp/image-text-pixmap.js
@@ -0,0 +1,6 @@
+Vue.component('image-text-pixmap', {
+    props: ['img'],
+    template: '<div class="ui-div"><h3>Pixmap JSON</h3></div>',
+    methods: {
+    }
+});
--- public/forpost/vuejs-example-image-edit/main.js
+++ public/forpost/vuejs-example-image-edit/main.js
@@ -1,8 +1,11 @@
 new Vue({
     el: '#app',
     template: '<div class="wrap_main">'+
-        '<image-color-pick v-bind:img="imgs[currentImage]" v-on:color-click="colorClickHandler"></image-color-pick>'+
-        '<image-div-grid v-bind:img="imgs[currentImage]" v-on:px-click="pxClickHandler"></image-div-grid>'+
+        '<div>'+
+            '<image-color-pick v-bind:img="imgs[currentImage]" v-on:color-click="colorClickHandler"></image-color-pick>'+
+            '<image-div-grid v-bind:img="imgs[currentImage]" v-on:px-click="pxClickHandler"></image-div-grid>'+
+        '</div>' +
+        '<image-text-pixmap v-bind:img="imgs[currentImage]"></image-text-pixmap>'+
     '</div>',
     data: function(){
         return {
