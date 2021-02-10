485ac33cff29e8e0dd9d2d92577a37b0d2801493
dustinpfister <dustin.pfister@gmail.com>
2021-02-10 10:07:56 -0500

    started keyup method

--- public/forpost/vuejs-example-image-edit/comp/image-text-pixmap.js
+++ public/forpost/vuejs-example-image-edit/comp/image-text-pixmap.js
@@ -8,7 +8,8 @@ Vue.component('image-text-pixmap', {
     template: '<div class="ui-div">'+
         '<h3>Pixmap JSON</h3>'+
         //'<button v-on:click="update">update</button>'+
-        '<textarea cols="70" rows="15">{{ json }}</textarea>'+
+        '<button v-on:click="load">load</button><br>'+
+        '<textarea cols="70" rows="15" v-text="json" v-on:keyup="keyup"></textarea>'+
     '</div>',
     mounted: function(){
         this.updateText();
@@ -18,12 +19,19 @@ Vue.component('image-text-pixmap', {
     },
     methods: {
         update: function(){
-            console.log('update text');
             this.updateText();
             this.$forceUpdate();
         },
+        keyup: function(e){
+            this.$data.json = e.target.value;
+            console.log(this.$data.json);
+        },
+        load: function(){
+            console.log(this.$data.json);
+            //this.$emit('load-json', this.$data.json);
+        },
         updateText : function(){
-            console.log(this.$props.imgs);
+            //console.log(this.$props.imgs);
             var pixmap = IMG.createPixmap({
                 imgs: this.$props.imgs
             });
--- public/forpost/vuejs-example-image-edit/todo.md
+++ public/forpost/vuejs-example-image-edit/todo.md
@@ -2,7 +2,9 @@
 
 
 ### 0.1.0 - first image text component
-* start a image-text-pixmap component that will be a JSON form of the image(s) (base on canvas-example-animation-pixmaps).
+* (done) start a image-text-pixmap component that will be a JSON form of the image(s) (base on canvas-example-animation-pixmaps).
+* add an load button that will set the state of the image to the json
+
 
 ### 0.0.0 - Basic idea working
 * (done) basic idea working
