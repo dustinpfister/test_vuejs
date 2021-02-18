4469d07c20e0536978cc4b1e6f7be782ed9789af
dustinpfister <dustin.pfister@gmail.com>
2021-02-18 09:23:59 -0500

    using sun-ui-pos in menu-sections-table menu comp

--- public/forpost/vuejs-example-land-sections/menus/sections-table.js
+++ public/forpost/vuejs-example-land-sections/menus/sections-table.js
@@ -1,6 +1,16 @@
 Vue.component('menu-sections-table', {
     props: ['currentMenu', 'sun', 'sections'],
     template: '<div v-if="currentMenu === \'sections-table\'">'+
+        '<sun-ui-pos ' +
+            'v-bind:currentMenu="$props.currentMenu" ' + 
+            'v-bind:sun="$props.sun" ' +
+            'v-bind:sections="$props.sections" ' +
+            'v-on:set-sunpos-ad="sunpos"></sun-ui-pos>'+
         '<sections-info-table v-bind:sections="sections"></sections-info-table>'+
-    '</div>'
+    '</div>',
+    methods:{
+        sunpos: function(a, b){
+            this.setPos(a, b)
+        }
+    }
 });
