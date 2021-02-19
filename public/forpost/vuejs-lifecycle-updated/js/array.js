d9388211ead5a968ea1a45e9f1afa6221565cd78
dustinpfister <dustin.pfister@gmail.com>
2021-02-19 09:52:40 -0500

    started an array example

---
+++ public/forpost/vuejs-lifecycle-updated/js/array.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+        el: '#demo',
+        template: '<div>' +
+            '<button v-on:click="startAWork">Start a Work</button>' +
+            '<h3> works:</h3>' +
+            '<div><div v-for="w in works" >id: {{w.id}}, worth: {{ w.worth }}</div></div>' +
+        '</div>',
+        data: {
+            count: 0,
+            works: []   // a log of work objects
+        },
+        updated: function () {
+            var data = this.$data;
+        },
+        methods: {
+            startAWork: function () {
+                var dat = this.$data;
+                var obj = {
+                    id: dat.count,
+                    worth: 1
+                };
+                dat.count += 1;
+                dat.works.push(obj);
+            }
+        }
+    });
