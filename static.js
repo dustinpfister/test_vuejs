2a9d50c69b1b4ac89fcbec19f2d4aa889f4db547
dustinpfister <dustin.pfister@gmail.com>
2021-02-21 11:48:36 -0500

    on second thought yes I should use path and dirname

--- static.js
+++ static.js
@@ -1,9 +1,10 @@
 // just a way to serve the html folder
 let express = require('express'),
+path = require('path'),
 app = express(),
 port = process.env.PORT || process.argv[2] || 8080;
  
-app.use('/', express.static('public'));
+app.use('/', express.static( path.resolve(__dirname, 'public') ));
 
 app.listen(port, function () {
     console.log('static server up on port: ' + port);
