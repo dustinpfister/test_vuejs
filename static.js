adf97f8462a6d1856d7179adb746c51aee064ac5
dustinpfister <dustin.pfister@gmail.com>
2021-02-21 12:25:09 -0500

    using serve index in static script

--- static.js
+++ static.js
@@ -1,5 +1,6 @@
 // just a way to serve the html folder
 let express = require('express'),
+serveIndex = require('serve-index'),
 path = require('path'),
 app = express(),
 PORT = process.env.PORT || process.argv[2] || 8080,
@@ -7,6 +8,8 @@ PUBLIC_HTML = path.resolve(__dirname, 'public');
 
 // use express static to serve the public folder
 app.use('/', express.static(PUBLIC_HTML));
+
+app.use('/forpost', serveIndex( path.resolve(PUBLIC_HTML, 'forpost') ))
 
 // listen on PORT
 app.listen(PORT, function () {
