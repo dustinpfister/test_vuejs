19ffc2694c41cb40588fcbe2f2196a00067f3613
dustinpfister <dustin.pfister@gmail.com>
2021-02-25 13:54:26 -0500

    using data option over created hook in main.js

--- public/forpost/vuejs-example-menu/main.js
+++ public/forpost/vuejs-example-menu/main.js
@@ -1,8 +1,11 @@
 var vm = new Menu({
     el: '#app',
+    data: {
+        menus: ['home', 'manual']
+    },
     created: function(){
-        var dat = this.$data;
-        dat.menus.push('manual')
+        //var dat = this.$data;
+        //dat.menus.push('manual');
     },
     methods:{
         // I can place methods that have to do with
@@ -11,6 +14,7 @@ var vm = new Menu({
             console.log('delta money event', a);
             this.$data.money += a;
         },
+        // custom state change method that will be called over Menu stateChange
         stateChange: function(key, value){
             console.log(key, value);
             if(key === 'delta-money'){
