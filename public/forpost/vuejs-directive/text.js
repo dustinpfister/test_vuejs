592f1f13ab11a3b55985d838f6c9bcec688fa547
dustinpfister <dustin.pfister@gmail.com>
2021-02-26 09:40:11 -0500

    renamed text to s1_text_bind in vue directive

--- public/forpost/vuejs-directive/text.js
+++
@@ -1,9 +0,0 @@
-
-var vm = new Vue({
-        el: '#header',
-        data: {
-            mess_h1: 'This is a v-text directive example',
-            mess_p: 'Uisng the v-text and v-bind:style directives here',
-            style_red: 'color:red;'
-        }
-    });
