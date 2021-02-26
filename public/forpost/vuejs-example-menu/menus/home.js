e6064a96bb90bde4afc8ff2e6df9a30f1aa7e80a
dustinpfister <dustin.pfister@gmail.com>
2021-02-26 09:40:47 -0500

    clean up of home.js in vuejs example menu

--- public/forpost/vuejs-example-menu/menus/home.js
+++ public/forpost/vuejs-example-menu/menus/home.js
@@ -1,5 +1,4 @@
 Vue.component('menu-home', {
-    //props: ['money', 'currentMenu'],
     props: ['state', 'currentMenu'],
     template: '<div v-if="currentMenu === \'home\'">'+
       '<p>This is home current number of clicks: {{ state.money }}</p>' +
